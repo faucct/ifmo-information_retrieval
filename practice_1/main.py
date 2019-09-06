442a8fb14f25360adcc72e646171c7aaa971783b
Nikita Sokolov <faucct@gmail.com>
2019-09-06 23:16:06 +0300

    Plain text should be extracted instead of markdown

--- practice_1/main.py
+++ practice_1/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.7
 from base64 import b64decode, b64encode
-from html2text import html2text
+import html_text
 from lxml import etree, html
 import os
 import urllib.parse
@@ -14,18 +14,19 @@ def urljoin(base, url):
 
 
 class HTML:
-    def __init__(self, element, html_bytes):
+    def __init__(self, element, html_bytes, tree):
         self.element = element
         self.bytes = html_bytes
+        self.tree = tree
 
     def text(self):
-        return html2text(self.bytes.decode("cp1251"))
+        return html_text.extract_text(self.tree)
 
     def urls(self):
         doc_url = b64decode(self.element.find('docURL').text).decode("cp1251")
         return [
             urljoin(doc_url, link_url)
-            for _, _, link_url, _ in html.document_fromstring(self.bytes).iterlinks()
+            for _, _, link_url, _ in self.tree.iterlinks()
         ]
 
 
@@ -34,7 +35,12 @@ class Document:
         self.element = element
 
     def html(self):
-        return HTML(self.element, b64decode(self.element.find('content').text))
+        html_bytes = b64decode(self.element.find('content').text)
+        return HTML(
+            self.element,
+            html_bytes,
+            html.document_fromstring(html_bytes, parser=html.HTMLParser(encoding='cp1251')),
+        )
 
 
 def iter_document_content(source):
@@ -59,6 +65,7 @@ if __name__ == "__main__":
     def partition_texts_base64(index):
         return os.path.join(byweb_for_course, f"texts.{index}.base64")
 
+
     def partition_url_base64(index):
         return os.path.join(byweb_for_course, f"urls.base64.{index}.csv")
 
@@ -76,15 +83,16 @@ if __name__ == "__main__":
             for document in tqdm(iter_document_content(partition_xml(index)), position=index):
                 print(b64encode(document.html().text().encode("utf-8")).decode(), file=output)
 
+
     def extract_urls(index):
         with open(partition_url_base64(index), "w") as output:
             for document in tqdm(iter_document_content(partition_xml(index)), position=index):
-                print((",".join(b64encode(url.encode("utf-8")).decode() for url in document.html().urls())), file=output)
+                print(",".join(b64encode(url.encode("utf-8")).decode() for url in document.html().urls()), file=output)
 
 
     partitions = range(10)
     freeze_support()
     Pool(len(partitions), initializer=tqdm.set_lock, initargs=(RLock(),)).map(
-        extract_urls,
+        extract_texts,
         partitions
     )
