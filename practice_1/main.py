5fa2c377aa51e11aac817ec4ac34620788c7ef2e
Nikita Sokolov <faucct@gmail.com>
2019-09-06 23:43:04 +0300

    HTML text ratios

--- practice_1/main.py
+++ practice_1/main.py
@@ -35,12 +35,15 @@ class Document:
         self.element = element
 
     def html(self):
-        html_bytes = b64decode(self.element.find('content').text)
+        html_bytes = self.content()
         return HTML(
             self.element,
             html_bytes,
             html.document_fromstring(html_bytes, parser=html.HTMLParser(encoding='cp1251')),
         )
+
+    def content(self):
+        return b64decode(self.element.find('content').text)
 
 
 def iter_document_content(source):
@@ -82,6 +85,10 @@ if __name__ == "__main__":
         return os.path.join(byweb_for_course, f"word_lengths.csv")
 
 
+    def html_text_ratios_csv():
+        return os.path.join(byweb_for_course, f"html_text_ratios.csv")
+
+
     def file_documents_number(index):
         return sum(1 for _ in tqdm(iter_document_content(partition_xml(index)), position=index))
 
@@ -108,12 +115,20 @@ if __name__ == "__main__":
             return [len(b64decode(text_base64).split()) for text_base64 in tqdm(input)]
 
 
+    def partition_html_text_ratios(index):
+        with open(partition_texts_base64(index)) as texts_base64:
+            return [
+                len(b64decode(text_base64).decode()) / len(document.content().decode('cp1251'))
+                for text_base64, document in tqdm(zip(texts_base64, iter_document_content(partition_xml(index))))
+            ]
+
+
     partitions = range(10)
     freeze_support()
-    all_word_lengths = sum(Pool(len(partitions), initializer=tqdm.set_lock, initargs=(RLock(),)).map(
-        partition_word_lengths,
+    all_html_text_ratios = sum(Pool(len(partitions), initializer=tqdm.set_lock, initargs=(RLock(),)).map(
+        partition_html_text_ratios,
         partitions
     ), [])
-    with open(word_lengths_csv(), "w") as output:
-        for byte_length in all_word_lengths:
-            print(byte_length, file=output)
+    with open(html_text_ratios_csv(), "w") as output:
+        for ratio in all_html_text_ratios:
+            print(ratio, file=output)
